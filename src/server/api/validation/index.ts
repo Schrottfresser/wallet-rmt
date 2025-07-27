c72dace5ab63b0dd3d778bd37ad00587e34e5806
Schrottfresser <39998368+Schrottfresser@users.noreply.github.com>
2025-07-28 01:16:20 +0200

    implement zod validation

---
+++ src/server/api/validation/index.ts
@@ -0,0 +1,35 @@
+import BadRequestError from "@server/errors/badRequestError.js";
+import { Request, Response, NextFunction, RequestHandler } from "express";
+import mongoose from "mongoose";
+import z from "zod";
+
+export const createValidatedHandler = <T extends z.ZodType>(
+    schema: T,
+    handler: (
+        data: z.infer<T>,
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => any
+): RequestHandler => {
+    return (req, res, next) => {
+        const parsed = schema.safeParse({
+            body: req.body,
+            query: req.query,
+            params: req.params,
+        });
+
+        if (!parsed.success) {
+            throw new BadRequestError(parsed.error.message);
+        }
+
+        return handler(parsed.data, req, res, next);
+    };
+};
+
+export const objectIdSchema = z
+    .string()
+    .refine((val) => mongoose.Types.ObjectId.isValid(val), {
+        message: "Invalid ObjectId",
+    })
+    .transform((val) => new mongoose.Types.ObjectId(val));
