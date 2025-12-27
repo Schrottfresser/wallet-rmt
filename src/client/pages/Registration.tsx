575fd3e683b4c125d0c4ea9289c22f47187d50ba
Schrottfresser <39998368+Schrottfresser@users.noreply.github.com>
2025-12-27 20:29:42 +0100

    add login and registration test frontend

---
+++ src/client/pages/Registration.tsx
@@ -0,0 +1,77 @@
+import { startAuthentication, startRegistration } from '@simplewebauthn/browser';
+import { useState } from 'react';
+
+export default function Registration() {
+    const [username, setUsername] = useState('test2');
+
+    async function register() {
+        const optionsResponse = await fetch('/api/user/webauthn/register/options', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username }),
+        });
+        const optionsJSON = await optionsResponse.json();
+
+        const attestationResponse = await startRegistration({ optionsJSON });
+
+        const verificationResp = await fetch('/api/user/webauthn/register/verify', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username,
+                attestationResponse,
+            }),
+        });
+        const verificationJSON = await verificationResp.json();
+
+        console.log(verificationJSON);
+    }
+
+    async function login() {
+        const optionsResponse = await fetch('/api/user/webauthn/login/options', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username }),
+        });
+        const optionsJSON = await optionsResponse.json();
+
+        const attestationResponse = await startAuthentication({ optionsJSON });
+
+        const verificationResp = await fetch('/api/user/webauthn/login/verify', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username,
+                attestationResponse,
+            }),
+        });
+        const verificationJSON = await verificationResp.json();
+
+        console.log(verificationJSON);
+    }
+
+    return (
+        <div className="mx-auto mt-5 flex w-min gap-2">
+            <input
+                type="text"
+                value="test2"
+                onChange={(event) => setUsername(event.target.value)}
+                className="border-2 p-2"
+            />
+            <button className="bg-green-700 px-5 py-2 hover:cursor-pointer" onClick={register}>
+                Register
+            </button>
+            <button className="bg-blue-700 px-5 py-2 hover:cursor-pointer" onClick={login}>
+                Login
+            </button>
+        </div>
+    );
+}
--- src/client/router.tsx
+++ src/client/router.tsx
@@ -1,9 +1,11 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes } from 'react-router';
+import Registration from './pages/Registration.js';
 
 function Router() {
     return (
         <Routes>
             <Route index element={<div></div>} />
+            <Route path="/register" Component={Registration} />
         </Routes>
     );
 }
