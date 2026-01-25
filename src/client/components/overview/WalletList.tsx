8c033c278f2f7276b83ae1c4fb6e2f8aedacd8af
Schrottfresser <39998368+Schrottfresser@users.noreply.github.com>
2026-01-25 21:11:03 +0100

    extract WalletListItem component

--- src/client/components/overview/WalletList.tsx
+++ src/client/components/overview/WalletList.tsx
@@ -1,18 +1,11 @@
 import Button from '@client/components/base/Button.js';
 import WalletCreateModal from '@client/components/overview/WalletCreateModal.js';
-import { getWalletIcon, formatBTC } from '@client/util/currency.js';
 import useWallets from '@client/hooks/useWallets.js';
-import {
-    ArrowPathIcon,
-    ArrowTurnDownLeftIcon,
-    LockClosedIcon,
-    LockOpenIcon,
-    PlusIcon,
-} from '@heroicons/react/24/solid';
+import { PlusIcon } from '@heroicons/react/24/solid';
 import { WalletDoc } from '@server/model/mongoose/wallet.js';
 import { ObjectId } from '@server/route/validation/index.js';
 import { useState } from 'react';
-import { Link } from 'react-router';
+import WalletListItem from '@client/components/overview/WalletListItem.js';
 
 interface WalletListProps {
     username: string;
@@ -50,48 +43,13 @@ function WalletList({ username }: Readonly<WalletListProps>) {
             <div className="lg:max-w-2/3 xl:max-w-1/2 mx-auto mt-5 p-3">
                 <h2 className="text-2xl font-bold">Wallet list</h2>
                 <ul>
-                    {wallets.data?.map((wallet) => {
-                        const WalletIcon = getWalletIcon(wallet.type);
-                        const LockIcon = wallet.isLoaded ? LockOpenIcon : LockClosedIcon;
-                        const lockButtonTitle = wallet.isLoaded ? 'Lock wallet' : 'Unlock wallet';
-
-                        const formattedBalance = formatBTC(wallet.balance || 0n);
-
-                        return (
-                            <li
-                                key={wallet._id.toString()}
-                                className="relative mt-2 p-3 rounded-md border-2 flex items-center gap-3 cursor-pointer hover:bg-gray-300"
-                            >
-                                <Link
-                                    to={`/wallet/${wallet._id}/`}
-                                    className="absolute left-0 top-0 w-full h-full rounded-md cursor-pointer"
-                                />
-
-                                <WalletIcon aria-label={wallet.type} className="size-10 shrink-0" />
-                                <span className="font-bold text-lg w-1/2 truncate">{wallet.name}</span>
-
-                                <span className="z-10 cursor-text text-nowrap">{formattedBalance}</span>
-
-                                <Button
-                                    style="solid"
-                                    icon={ArrowPathIcon}
-                                    title="Refresh wallet"
-                                    disabled={!wallet.isLoaded}
-                                    onClick={() => handleRefreshClick(wallet._id)}
-                                    className="text-lg z-10 ml-auto"
-                                />
-                                <Button
-                                    style="solid"
-                                    icon={LockIcon}
-                                    title={lockButtonTitle}
-                                    onClick={() => handleLockClick(wallet)}
-                                    className="text-lg z-10"
-                                />
-
-                                <ArrowTurnDownLeftIcon className="size-8" />
-                            </li>
-                        );
-                    })}
+                    {wallets.data?.map((wallet) => (
+                        <WalletListItem
+                            wallet={wallet}
+                            handleRefreshClick={handleRefreshClick}
+                            handleLockClick={handleLockClick}
+                        />
+                    ))}
                 </ul>
                 <Button
                     style="solid"
---
+++ src/client/components/overview/WalletListItem.tsx
@@ -0,0 +1,58 @@
+import Button from '@client/components/base/Button.js';
+import { formatBTC, getWalletIcon } from '@client/util/currency.js';
+import { ArrowPathIcon, ArrowTurnDownLeftIcon, LockClosedIcon, LockOpenIcon } from '@heroicons/react/24/solid';
+import { WalletDoc } from '@server/model/mongoose/wallet.js';
+import { ObjectId } from '@server/route/validation/index.js';
+import { useMemo } from 'react';
+import { Link } from 'react-router';
+
+interface WalletListItemProps {
+    wallet: WalletDoc;
+    handleRefreshClick: (walletId: ObjectId) => void;
+    handleLockClick: (wallet: WalletDoc) => void;
+}
+
+function WalletListItem({ wallet, handleRefreshClick, handleLockClick }: WalletListItemProps) {
+    const WalletIcon = useMemo(() => getWalletIcon(wallet.type), [wallet.type]);
+    const formattedBalance = useMemo(() => formatBTC(wallet.balance || 0n), [wallet.balance]);
+
+    const LockIcon = wallet.isLoaded ? LockOpenIcon : LockClosedIcon;
+    const lockButtonTitle = wallet.isLoaded ? 'Lock wallet' : 'Unlock wallet';
+
+    return (
+        <li
+            key={wallet._id.toString()}
+            className="relative mt-2 p-3 rounded-md border-2 flex items-center gap-3 cursor-pointer hover:bg-gray-300"
+        >
+            <Link
+                to={`/wallet/${wallet._id}/`}
+                className="absolute left-0 top-0 w-full h-full rounded-md cursor-pointer"
+            />
+
+            <WalletIcon aria-label={wallet.type} className="size-10 shrink-0" />
+            <span className="font-bold text-lg w-1/2 truncate">{wallet.name}</span>
+
+            <span className="z-10 cursor-text text-nowrap">{formattedBalance}</span>
+
+            <Button
+                style="solid"
+                icon={ArrowPathIcon}
+                title="Refresh wallet"
+                disabled={!wallet.isLoaded}
+                onClick={() => handleRefreshClick(wallet._id)}
+                className="text-lg z-10 ml-auto"
+            />
+            <Button
+                style="solid"
+                icon={LockIcon}
+                title={lockButtonTitle}
+                onClick={() => handleLockClick(wallet)}
+                className="text-lg z-10"
+            />
+
+            <ArrowTurnDownLeftIcon className="size-8" />
+        </li>
+    );
+}
+
+export default WalletListItem;
