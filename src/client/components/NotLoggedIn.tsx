cdcff94f5700d5d82ffa4c76d911ab7d4696bfdc
Schrottfresser <39998368+Schrottfresser@users.noreply.github.com>
2026-01-25 03:54:44 +0100

    refactor NotLoggedIn component

--- src/client/components/NotLoggedIn.tsx
+++ src/client/components/NotLoggedIn.tsx
@@ -2,12 +2,15 @@ import { ExclamationTriangleIcon } from '@heroicons/react/24/solid';
 
 function NotLoggedIn() {
     return (
-        <div className="absolute top-1/2 left-1/2 -translate-1/2 w-1/4">
-            <div className="flex gap-3">
-                <ExclamationTriangleIcon className="size-8" />
-                <h2 className="text-xl font-bold">Not logged in</h2>
+        <div className="absolute h-[calc(100%-var(--header-height))] w-full flex p-10">
+            <div className="mx-auto my-auto w-fit">
+                <div className="flex gap-3">
+                    <ExclamationTriangleIcon className="size-8" />
+                    <h2 className="text-xl font-bold">Not logged in</h2>
+                </div>
+                <p className="mt-2">You were logged out of the application.</p>
+                <p>Only one session is permitted per user simultaneously.</p>
             </div>
-            <p className="mt-2">Please login in order to use the application.</p>
         </div>
     );
 }
